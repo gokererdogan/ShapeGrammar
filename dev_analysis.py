4f8b9fdaed997beaa462b53ba8699b7299d9750f
gerdogan <gokererdogan@gmail.com>
2013-07-08 14:29:41 -0400

    Haptic forward model updated to include more grasps.
    We collect 24 (instead of 8) grasps from GraspIt since we need the
    haptic information to be more informative. This commit includes
    minor changes to haptic forward model for this purpose. There are
    also other minor, non-significant changes in a few other places.

--- dev_analysis.py
+++ dev_analysis.py
@@ -8,17 +8,26 @@ from mcmc_sampler import *
 from vision_forward_model import VisionForwardModel
 
 # load results
-fname = 'AoMRShapeGrammar_Visual_Obj120130620205934.mcmc'
+fname = 'AoMRShapeGrammar_Visual_Obj820130624004057.mcmc'
 f = open(fname)
 results = pickle.load(f)
 print results
+
+print(results.best_samples[0][0].state.ll_params)
+
 forward_model = VisionForwardModel()
+i = 0
 for sample in results.best_samples[0]:
     sample.state.tree.show()
-    forward_model._view(sample.state)
+    #forward_model._view(sample.state)
+    forward_model.save_image('best_sample'+str(i)+'.png', sample.state)
+    i += 1
 
 print('\n------------------------------------\nSamples')
+i = 0
 for sample in results.samples[0]:
-    forward_model._view(sample.state)
+    #forward_model._view(sample.state)
     sample.state.tree.show()
+    forward_model.save_image('sample'+str(i)+'.png', sample.state)
+    i += 1
 
---
+++ run_aomr.sh
@@ -0,0 +1,15 @@
+# python run_aomr_grammar.py 2 1600 v
+# python run_aomr_grammar.py 3 1500 v
+# python run_aomr_grammar.py 4 1600 v
+# python run_aomr_grammar.py 5 1300 v
+# python run_aomr_grammar.py 6 1500 v
+# python run_aomr_grammar.py 7 1200 v
+# python run_aomr_grammar.py 8 1200 v
+# python run_aomr_grammar.py 9 1200 v
+python run_aomr_grammar.py 10 1500 v
+python run_aomr_grammar.py 11 1200 v
+python run_aomr_grammar.py 12 1500 v
+python run_aomr_grammar.py 13 1200 v
+python run_aomr_grammar.py 14 1400 v
+python run_aomr_grammar.py 15 1200 v
+#python run_aomr_grammar.py 1 6000 		
